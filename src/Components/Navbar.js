c74c47cb980347fb53b59cfbaf695d2d450ac62d
Siddhant <siddhant.rai.5686@gmail.com>
2022-05-10 11:40:36 +0530

    Fixes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,11 @@
-import React, {useState,useEffect} from 'react'
-import {IoReloadOutline} from "react-icons/io5";
+import React, { useState, useEffect } from 'react'
 const API_KEY = '368d3bb235c625628fa868ed2b2c797e';
 
 function Navbar(props) {
     const [city, setCity] = useState(null);
-    const submit = (e)=>{
+    const submit = (e) => {
         e.preventDefault();
-        async function getWeatherSearch(){
+        async function getWeatherSearch() {
             let data = await getWeather(city)
             props.parentCallback(data)
         }
@@ -14,82 +13,62 @@ function Navbar(props) {
     }
     return (
         <>
-        <header className="navbar">
-            <div
-                className="flex items-center justify-between h-16  px-1 mx-auto"
-            >
-                <div className="flex flex-1 w-0 lg:hidden">
-                    <button className="p-2 text-gray-600 bg-gray-100 rounded-full" type="button">
-                        <svg
-                            className="w-5 h-5"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                            ></path>
-                        </svg>
-                    </button>
-                </div>
-
-                <div className="logo">
-                    <span className="logo  px-2 py-2 rounded" style={{color: "white"}}>WeatherToday</span>
-                </div>
-
-                <nav
-                    className="items-center justify-center hidden space-x-8 text-sm font-medium lg:flex lg:flex-1 lg:w-0"
+            <header className="navbar">
+                <div
+                    className="flex items-center justify-between h-16  px-1 mx-auto"
                 >
-                </nav>
 
-                <div className="items-center hidden space-x-4 lg:flex">
-                    <form className="hidden mb-0 lg:flex" onSubmit={submit}>
-                        <div className="relative" onSubmit={submit}>
-                            <input
-                                className="h-10 pr-10 text-sm placeholder-gray-300 border-gray-200 rounded-lg focus:z-10 position-relative"
-                                placeholder="  Search city..."
-                                type="text"
-                                onChange={(e) => setCity(e.target.value)}
-                            />
 
-                            <button
-                                className="absolute inset-y-0 right-0 p-2 mr-px text-gray-600 rounded-r-lg"
-                                type="submit"
-                                onSubmit={submit}
-                            >
-                                <svg
-                                    className="w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        clipRule="evenodd"
-                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                        fillRule="evenodd"
-                                    ></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
-                    <a
-                        className="px-3 py-2 text-sm font-medium text-white bg-blue-800 rounded-full drop-shadow-md"
-                        href=""
+                    <div className="logo">
+                        <span className="logo  px-2 py-2 rounded" style={{ color: "white" }}>WeatherToday</span>
+                    </div>
+
+                    <nav
+                        className="items-center justify-center hidden space-x-8 text-sm font-medium lg:flex lg:flex-1 lg:w-0"
                     >
-                        <IoReloadOutline size={25}/>
-                    </a>
+                    </nav>
+
+                        <div className="items-center hidden space-x-4 lg:flex">
+                            <form className="hidden mb-0 lg:flex" onSubmit={submit}>
+                                <div className="relative" onSubmit={submit}>
+                                    <input
+                                        className="h-10 pr-10 text-sm placeholder-gray-300 border-gray-200 rounded-lg focus:z-10 position-relative"
+                                        placeholder="  Search city..."
+                                        type="text"
+                                        onChange={(e) => setCity(e.target.value)}
+                                    />
+
+                                    <button
+                                        className="absolute inset-y-0 right-0 p-2 mr-px text-gray-600 rounded-r-lg"
+                                        type="submit"
+                                        onSubmit={submit}
+                                    >
+                                        <svg
+                                            className="w-5 h-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                clipRule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                fillRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    
                 </div>
-            </div>
-        </header>
+
+            </header>
+
         </>
     )
 }
 
-async function getGeocode(cityName){
+async function getGeocode(cityName) {
     let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
     let data = await response.json();
     let lat1 = `${data[0].lat}`;
